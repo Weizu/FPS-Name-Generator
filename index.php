333b6c01c34115bc4e7f636cbd30e188970f0ab1
Alexandre Achain (Weizu) <alexandre.achain@gmail.com>
2021-04-15 17:45:49 +0200

    Améliorations visuelles basiques
    Mise à jour faites lors de la présentation du HTML et CSS sur la chaîne Twitch de la communauté Multi Tech Live : https://www.twitch.tv/multi_tech_lab

--- index.php
+++ index.php
@@ -3,17 +3,27 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style/style.css">
     <title>Générateur de titres absurde</title>
+
+    <meta name="description" content="LE générateur officiel du S.L.I.P.">
 </head>
-<body>  
+<body>
+    <header>
+        <h1>Générateur de titres de <abbr lang="en" title="First Person Shooter">F.P.S</abbr> du <abbr title="Syndicat des Logiciels Interactifs et Pédagogiques">S.L.I.P</abbr></h1>
+    </header>
     <main>
-        <h1><?php echo genererTitre(); ?></h1>
-    </main>    
+        <section>
+            <span class="titrefps"><?php echo genererTitre(); ?></span>
+        </section>
+    </main>
+    <footer>
+        <a href="http://weizu.chez.com" target="_blank">Chez Weizu ! (nouvel onglet)</a>
+    </footer>
 </body>
 </html>
